d1d7d01aef69cf151b6cf04a6cc9c0fc5058acad
LucasButto <lbutto40@gmail.com>
2023-09-01 19:48:41 -0300

    Practica 3 Entity framework completa

---
+++ labNetPractica3/PracticaEF.Logic/CategoriesLogic.cs
@@ -0,0 +1,25 @@
+﻿using PracticaEF.Data;
+using PracticaEF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaEF.Logic
+{
+    public class CategoriesLogic
+    {
+        private readonly NorthwindContext _context;
+
+        public CategoriesLogic()
+        {
+            _context = new NorthwindContext();
+        }
+
+        public List<Categories> GetAll()
+        {
+            return _context.Categories.ToList();
+        }
+    }
+}
